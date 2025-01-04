094a8f44ea44d1af848d4919e4c6232e7d4bbb1d
tomashagos <tomashagos19@gmail.com>
2025-01-03 21:48:03 -0600

    Added a function and a demo

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 for i in range(0,7):
   print(i)
 
-print("Hello World")
+def find_value(num,list):
+  for i in range(0,len(list)):
+    if(list[i]==num):
+      return True
+    
+  return False
+
+
+
+my_list = [1,34,4,5,2]
+print(find_value(5,my_list))
+
